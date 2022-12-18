6edd7d8824a609db95dfe63688b5e97a87e7c225
JoseGuti <2146130@cegepoutaouais.qc.ca>
2022-12-18 01:28:09 -0500

    Reservation par le client existant

--- ProjetSynthese/Forms/Reservation.Designer.cs
+++ ProjetSynthese/Forms/Reservation.Designer.cs
@@ -32,6 +32,7 @@
             this.comboBoxRes = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.labelErr = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBoxRes
@@ -72,11 +73,24 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // labelErr
+            // 
+            this.labelErr.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelErr.AutoSize = true;
+            this.labelErr.Location = new System.Drawing.Point(172, 253);
+            this.labelErr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelErr.Name = "labelErr";
+            this.labelErr.Size = new System.Drawing.Size(0, 13);
+            this.labelErr.TabIndex = 16;
+            // 
             // Reservation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(630, 370);
+            this.Controls.Add(this.labelErr);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBoxRes);
@@ -94,5 +108,6 @@
         private System.Windows.Forms.ComboBox comboBoxRes;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelErr;
     }
 }
--- ProjetSynthese/Forms/Reservation.cs
+++ ProjetSynthese/Forms/Reservation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,18 +23,133 @@ namespace ProjetSynthese.Forms
 
         }
 
+        /// <summary>
+        /// Methode pour changer le statut d'occupé dans la chambre
+        /// </summary>
+        /// <param name="ch"></param>
+        public void ChangerStatutChambre(Chambre ch)
+        {
+            // on change le status de la chambre à true pour dire qu'elle est prise
+            ch.Status = true;
+            //On change les valeur dans la base de données pour que la chambre apparaise comme occupée
+            //On instacie aussi cet evenement dans la "troisième" table qui suit la trace du numero de reservation et du numero du client
+            SqlDataReader resultat = Static_Autentification.OuvrirConnectionBase("UPDATE Chambres SET [Status] = 1 WHERE NumeroReservation = '"
+                + ch.Num_Reservation + "'" + "\nINSERT INTO Reservations\nVALUES\t('" + ch.Num_Reservation + "', " + c.Num_client + ")");
+
+            resultat.Close();
+            
+            //Afficher la facture du client avec ses informations et le numero de sa chambre
+            MessageBox.Show("Voici votre facture \n" + c.ToString() + "\nChambre: " + ch.Num_Reservation +
+                "\n\nMerci d'avoir fait affaire avec Hotel Jose!");
+            this.Close();
+        }
+
+        /// <summary>
+        /// Methode pour changer le statut d'occupé dans la salle
+        /// </summary>
+        /// <param name="sl"></param>
+        public void ChangerStatutSalle(Salle sl)
+        {
+            // on change le status de la salle à true pour dire qu'elle est prise
+            sl.Status = true;
+            //On change les valeur dans la base de données pour que la salle apparaise comme occupée
+            //On instacie aussi cet evenement dans la "troisième" table qui suit la trace du numero de reservation et du numero du client
+            SqlDataReader resultat = Static_Autentification.OuvrirConnectionBase("UPDATE Salles SET [Status] = 1 WHERE NumeroReservation = '"
+                + sl.Num_Reservation + "'" + "\nINSERT INTO Reservations\nVALUES\t('" + sl.Num_Reservation + "', " + c.Num_client + ")");
+
+            resultat.Close();
+            //Afficher la facture du client avec ses informations et le numero de sa salle
+            MessageBox.Show("Voici votre facture \n" + c.ToString() + "\nSalle: " + sl.Num_Reservation +
+                "\n\nMerci d'avoir fait affaire avec Hotel Jose!");
+            this.Close();
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
+            labelErr.Text = string.Empty;
+            bool bl_mess = true;
             if (comboBoxRes.Text != "Cliquez pour derrouler les options")
             {
+                
+                
                 if (comboBoxRes.Text == "Chambre: Suite" || comboBoxRes.Text ==  "Chambre: Régulière")
                 {
-                    Chambre ch;
-                    
+                    //Si c'est une suite ou une reguliere on passe à travers la liste des chambres
+                   foreach (Chambre ch in Static_GererReservations.LsChambre)
+                    {
+                        //Si on trouve une chambre de type Suite libre
+                        if (comboBoxRes.Text == "Chambre: Suite" && ch.Type == "Suite" && ch.Status == false)
+                        {
+                            ChangerStatutChambre(ch);
+                            bl_mess = false;
+                            break;
+                        }
+                        //Si on trouve une chambre de type Suite Régulière
+                        else if (comboBoxRes.Text == "Chambre: Régulière" && ch.Type == "Régulière" && ch.Status == false)
+                        {
+                            ChangerStatutChambre(ch);
+                            bl_mess = false;
+                            break;
+                        }
+                    }
+                   //Afficher un message d'erreur si toutes les chambres sont prises ou (toutes ont un statut true)
+                    if (bl_mess == true)
+                    {
+                        labelErr.ForeColor = Color.Red;
+                        labelErr.Text = "Aucune chambre de ce type n'est disponible pour réserver";
+                    }
                 }
+                else if (comboBoxRes.Text == "Salle: Piscine" || comboBoxRes.Text == "Salle: Cinema" || comboBoxRes.Text == "Salle: Salle de reunions")
+                {
+                    foreach (Salle sl in Static_GererReservations.LsSalle)
+                    {
+                        //Si on trouve une salle du nom Piscine libre
+                        if (comboBoxRes.Text == "Salle: Piscine" && sl.Nom == "Piscine" && sl.Status == false)
+                        {
+                            ChangerStatutSalle(sl);
+                            bl_mess = false;
+                            break;
+                        }
+                        //Si on trouve une salle du nom Cinema libre (il faut specifier A ou B, car il va chercher le nom exact de salle)
+                        //Tandis qu'avec les chambres il allais chercher la premiere instance.
+                        else if (comboBoxRes.Text == "Salle: Cinema" && sl.Nom == "Cinema A" && sl.Status == false)
+                        {
+                            ChangerStatutSalle(sl);
+                            bl_mess = false;
+                            break;
+                        }
+                        else if (comboBoxRes.Text == "Salle: Cinema" && sl.Nom == "Cinema B" && sl.Status == false)
+                        {
+                            ChangerStatutSalle(sl);
+                            bl_mess = false;
+                            break;
+                        }
+                        //Si on trouve une salle du nom Salle de reunions libre
+                        //même procedure qu'avec cinema
+                        else if (comboBoxRes.Text == "Salle: Salle de reunions" && sl.Nom == "Salle de reunions A" && sl.Status == false)
+                        {
+                            ChangerStatutSalle(sl);
+                            bl_mess = false;
+                            break;
+                        }
+                        else if (comboBoxRes.Text == "Salle: Salle de reunions" && sl.Nom == "Salle de reunions B" && sl.Status == false)
+                        {
+                            ChangerStatutSalle(sl);
+                            bl_mess = false;
+                            break;
+                        }
+                    }
+                    //Afficher un message d'erreur si toutes les salles sont prises ou (toutes ont un statut true)
+                    if (bl_mess == true)
+                    {
+                        labelErr.ForeColor = Color.Red;
+                        labelErr.Text = "Aucune Salle de avec cette description n'est disponible pour réserver";
+                    }
+                }
+
+
             }
-            MessageBox.Show("Voici votre facture \n" + c.ToString() + "\n\nMerci d'avoir fait affaire avec Hotel Jose!");
-            this.Close(); 
+            
         }
 
         //Instanciation d'un objet client pour l'amener dans ce formulaire avec la methode au dessus
